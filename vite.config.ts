b6aa9216981b79624551669b84be5090d64ee217
olandatonick <olandatonick@gmail.com>
2025-12-29 07:09:28 +0000

    fix(pwa): Improve offline support
    Adds a navigateFallback to index.html and includes more asset types in the
    service worker cache. This ensures that the PWA can be used offline and
    that all pages are loaded correctly.

--- vite.config.ts
+++ vite.config.ts
@@ -28,6 +28,10 @@ export default defineConfig(({ mode }) => {
               },
             ],
           },
+          workbox: {
+            globPatterns: ['**/*.{js,css,html,ico,png,svg,jpg,jpeg,gif}'],
+            navigateFallback: '/index.html',
+          }
         }),
       ],
       define: {
